1000594129e6b7dc20fba7de637b093d5da166cd
liuxiaochuang <1932671565@qq.com>
2018-01-05 16:48:08 +0800

    新增文件

---
+++ src/com/zhiyou100/other/Test02.java
@@ -0,0 +1,16 @@
+package com.zhiyou100.other;
+
+/**  
+* @ClassName: Test02  
+* @Description: TODO
+* @author Administrator  
+* @date 2018年1月5日  下午4:46:41
+*  
+*/
+public class Test02 {
+
+	public static void main(String[] args) {
+		System.out.println("新增文件");
+	}
+}
+
